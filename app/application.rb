b10864a87887f049079286572505ad4257b446fb
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-14 16:33:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,8 +18,7 @@ class Application
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
-      end
-    end
+  
 
     elsif req.path.match(/add/)
     			item = req.params["item"]
